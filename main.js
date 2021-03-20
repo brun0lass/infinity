77c9dd3da603ce8c6fa0dda0e6be4582b754085c
thzzin <49045370+thzzin@users.noreply.github.com>
2021-03-20 05:24:09 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+
+setTimeout(function(){
+    var node = document.getElementById('apagar')
+    node.remove()
+    var aparece = document.getElementById('aparece')
+    aparece.style.display = "block"
+},5500)
+
