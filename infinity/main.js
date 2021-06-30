809c59ff41ea2ff83992ec4287295338bd5ed35e
thzzin <thithipaiva@hotmail.com>
2021-06-30 14:11:56 -0300

    novo index

--- infinity/main.js
+++
@@ -1,8 +0,0 @@
-
-setTimeout(function(){
-    var node = document.getElementById('apagar')
-    node.remove()
-    var aparece = document.getElementById('aparece')
-    aparece.style.display = "block"
-},5500)
-
